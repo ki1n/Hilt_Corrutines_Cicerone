ecf96e6de15b803eae6d4e4249b1400c2af01ca9
Turyev Nikolay <51500324+ki1n@users.noreply.github.com>
2022-04-29 10:52:03 +0600

    added logic to zoom in and out of the image

--- app/src/main/java/ru/turev/hiltcorrutinescicerone/ui/image_photo/ImagePhotoFragment.kt
+++ app/src/main/java/ru/turev/hiltcorrutinescicerone/ui/image_photo/ImagePhotoFragment.kt
@@ -1,9 +1,14 @@
 package ru.turev.hiltcorrutinescicerone.ui.image_photo
 
+import android.graphics.Bitmap
+import android.graphics.drawable.Drawable
 import android.os.Bundle
 import android.view.View
 import androidx.core.os.bundleOf
 import androidx.fragment.app.viewModels
+import com.bumptech.glide.Glide
+import com.bumptech.glide.request.target.CustomTarget
+import com.bumptech.glide.request.transition.Transition
 import dagger.hilt.android.AndroidEntryPoint
 import ru.turev.hiltcorrutinescicerone.R
 import ru.turev.hiltcorrutinescicerone.databinding.FragmentImagePhotoBinding
@@ -30,7 +35,23 @@ class ImagePhotoFragment : BaseFragment(R.layout.fragment_image_photo) {
     }
 
     private fun initData() {
-        binding.imagePhotoView.setData(itemPhoto)
+        with(binding) {
+            Glide.with(imagePhotoView.context)
+                .asBitmap()
+                .load(itemPhoto.full)
+                .into(object : CustomTarget<Bitmap>() {
+                    override fun onResourceReady(resource: Bitmap, transition: Transition<in Bitmap>?) {
+                        imagePhotoView.setData(resource)
+                    }
+
+                    override fun onLoadCleared(placeholder: Drawable?) {
+                        // this is called when imageView is cleared on lifecycle call or for
+                        // some other reason.
+                        // if you are referencing the bitmap somewhere else too other than this imageView
+                        // clear it here as you can no longer have the bitmap
+                    }
+                })
+        }
     }
 
     companion object {
--- app/src/main/java/ru/turev/hiltcorrutinescicerone/view/ImagePhotoView.kt
+++ app/src/main/java/ru/turev/hiltcorrutinescicerone/view/ImagePhotoView.kt
@@ -1,21 +1,113 @@
 package ru.turev.hiltcorrutinescicerone.view
 
 import android.content.Context
+import android.graphics.Bitmap
+import android.graphics.Canvas
+import android.graphics.Paint
 import android.util.AttributeSet
+import android.util.Log
+import android.view.MotionEvent
+import android.view.ScaleGestureDetector
 import androidx.appcompat.widget.AppCompatImageView
-import com.bumptech.glide.Glide
-import ru.turev.hiltcorrutinescicerone.domain.entity.ItemPhoto
 
 class ImagePhotoView @JvmOverloads constructor(
     context: Context,
-    attrs: AttributeSet? = null,
+    attrs: AttributeSet?,
     defStyleAttr: Int = 0,
 ) : AppCompatImageView(context, attrs, defStyleAttr) {
 
-    fun setData(itemPhoto: ItemPhoto) {
-        Glide.with(this.context)
-            .load(itemPhoto.full)
-            .into(this)
+    companion object {
+        private const val INVALID_POINTER_ID = -1
     }
 
+    private var bitmap: Bitmap? = null
+    private val paint: Paint = Paint().apply { isFilterBitmap = true }
+    private var posX = 0f
+    private var posY = 0f
+    private var lastTouchX = 0f
+    private var lastTouchY = 0f
+    private var activePointerId = INVALID_POINTER_ID
+    private val scaleDetector: ScaleGestureDetector
+    private var scaleFactor = 1f
+
+    override fun onTouchEvent(event: MotionEvent): Boolean {
+        scaleDetector.onTouchEvent(event)
+        val action = event.action
+        when (action and MotionEvent.ACTION_MASK) {
+            MotionEvent.ACTION_DOWN -> {
+                val x = event.x
+                val y = event.y
+                lastTouchX = x
+                lastTouchY = y
+                activePointerId = event.getPointerId(0)
+            }
+            MotionEvent.ACTION_MOVE -> {
+                val pointerIndex = event.findPointerIndex(activePointerId)
+                val x = event.getX(pointerIndex)
+                val y = event.getY(pointerIndex)
+
+                if (!scaleDetector.isInProgress) {
+                    val dx = x - lastTouchX
+                    val dy = y - lastTouchY
+                    posX += dx
+                    posY += dy
+                    invalidate()
+                }
+                lastTouchX = x
+                lastTouchY = y
+            }
+            // поднял палец
+            MotionEvent.ACTION_UP -> {
+                activePointerId = INVALID_POINTER_ID
+            }
+            // если палец выходит за пределы экрана
+            MotionEvent.ACTION_CANCEL -> {
+                activePointerId = INVALID_POINTER_ID
+            }
+            MotionEvent.ACTION_POINTER_UP -> {
+                val pointerIndex = (event.action and MotionEvent.ACTION_POINTER_INDEX_MASK
+                        shr MotionEvent.ACTION_POINTER_INDEX_SHIFT)
+                val pointerId = event.getPointerId(pointerIndex)
+                if (pointerId == activePointerId) {
+                    val newPointerIndex = if (pointerIndex == 0) 1 else 0
+                    lastTouchX = event.getX(newPointerIndex)
+                    lastTouchY = event.getY(newPointerIndex)
+                    activePointerId = event.getPointerId(newPointerIndex)
+                }
+            }
+        }
+        return true
+    }
+
+    override fun onDraw(canvas: Canvas) {
+        super.onDraw(canvas)
+        canvas.save()
+        Log.d("tag", "X: $posX Y: $posY")
+        canvas.translate(posX, posY)
+        canvas.scale(scaleFactor, scaleFactor)
+        // todo корректно отобразить надо, исправить
+        bitmap?.let { canvas.drawBitmap(it, 0f, 100f, paint) }
+
+        canvas.restore()
+    }
+
+    // Детектор жестов масштаба
+    private inner class ScaleListener : ScaleGestureDetector.SimpleOnScaleGestureListener() {
+        override fun onScale(detector: ScaleGestureDetector): Boolean {
+            scaleFactor *= detector.scaleFactor
+
+            scaleFactor = 0.1f.coerceAtLeast(scaleFactor.coerceAtMost(10.0f))
+            invalidate()
+            return true
+        }
+    }
+
+    init {
+        scaleDetector = ScaleGestureDetector(context, ScaleListener())
+    }
+
+    fun setData(bitmap: Bitmap) {
+        this.bitmap = bitmap
+        invalidate()
+    }
 }
